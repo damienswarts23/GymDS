7c12c9b11359c00ba8663291160b4635aa5285ff
Damien Swarts <222868791@mycput.ac.za>
2025-12-03 17:18:12 +0200

    I added comments and I added code that deletes the users information incase the user wants to cancel signing up.

--- gymApp/Signup2.cs
+++ gymApp/Signup2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace gymApp
 {
@@ -19,9 +21,52 @@ namespace gymApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SignUp s = new SignUp();
-            s.Show();
-            this.Hide();
+            DBConnection db = new DBConnection();//object of database class
+
+            DialogResult result = MessageBox.Show(
+
+                "Are you sure you want to quite, Your records will be deleted", // message
+                "Confirm",                                   // title
+                MessageBoxButtons.YesNo,                     // buttons
+                MessageBoxIcon.Question                      // icon
+            );
+
+            // Check what the user clicked
+            if (result == DialogResult.Yes)
+            {
+
+                try
+                {
+                    // Use your connection class
+                    using (MySqlConnection conn = db.Connect())
+                    {
+                        //This is the sql query that will place the data into the database
+                        string query = "DELETE FROM members ORDER BY member_id DESC LIMIT 1";
+                        using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                        {
+                            int rowsAffected = cmd.ExecuteNonQuery();//executing the command so that it can happen in the database
+                            MessageBox.Show(rowsAffected > 0 ? "Your details has been deleted." : "Details is not deleted.");
+                        }
+                    }
+
+                    //This is only done so that i can move on to the next page
+                    Form1 s2 = new Form1();
+                    s2.Show();
+                    this.Hide();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+
+            }
+            else
+            {
+                // User clicked No → cancel action
+                MessageBox.Show("Operation cancelled.");
+            }
+
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
